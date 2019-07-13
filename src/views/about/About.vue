8447dd7fb881d025fc000aedde38f0666be9995b
luoyangC <luoyang15261829198@gmail.com>
2019-07-13 16:12:04 +0800

    style: 优化显示效果
    修改了部分css，优化页面显示效果

--- src/views/about/About.vue
+++ src/views/about/About.vue
@@ -3,3 +3,13 @@
     <h1>This is an about page</h1>
   </div>
 </template>
+
+<style lang="less" scoped>
+.about{
+  background: #fff;
+  h1 {
+    margin: 0;
+    padding: 16px;
+  }
+}
+</style>
--- src/views/errors/404.vue
+++ src/views/errors/404.vue
@@ -1,7 +1,30 @@
 <template>
-  <div>
+  <div class="error">
     <p>页面被噬元兽吞噬了</p>
-    <van-button icon="arrow-left" type="info">返回</van-button>
-    <van-button icon="home-o" type="info">首页</van-button>
+    <van-button icon="arrow-left" type="info" @click="onBack">返回</van-button>
+    <van-button icon="home-o" type="info" to="/home">首页</van-button>
   </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    onBack() {
+      this.$router.go(-1)
+    }
+  }
+}
+</script>
+
+
+<style lang="less" scoped>
+.error {
+  background: #fff;
+  text-align: center;
+  margin-top: 26px;
+  .van-button {
+    margin: 16px;
+  }
+}
+</style>
+
