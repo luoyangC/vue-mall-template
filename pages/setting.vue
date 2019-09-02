935c02eaa685954e1a9497df2bfa73c6c62d0727
luoyangC <luoyang15261829198@gmail.com>
2019-09-02 11:06:22 +0800

    feat: 新增关于入口

--- pages/setting.vue
+++ pages/setting.vue
@@ -1,15 +1,22 @@
 <template>
   <div>
+    <vant-cell-group>
+      <van-cell title="关于" is-link to="/about" />
+    </vant-cell-group>
     <van-button type="default" style="width: 100%" @click="handleLogout">退出登录</van-button>
   </div>
 </template>
 
 <script>
-
+import { Cell, CellGroup } from 'vant'
 export default {
   name: 'Setting',
   layout: 'general',
   middleware: 'auth',
+  components: {
+    [CellGroup.name]: CellGroup,
+    [Cell.name]: Cell
+  },
   methods: {
     handleLogout() {
       this.$store.dispatch('user/logout')
