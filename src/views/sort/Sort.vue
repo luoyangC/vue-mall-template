a807ca67bc65c8fbaaedb9e06f50fbe574602232
luoyangC <luoyang15261829198@gmail.com>
2019-09-02 14:16:21 +0800

    refactor: 重构分类页

--- src/views/sort/Sort.vue
+++ src/views/sort/Sort.vue
@@ -1,13 +1,55 @@
 <template>
-  <sort-sidebar />
+  <van-tree-select
+    height="92.5vh"
+    :items="items"
+    :main-active-index.sync="mainActiveIndex"
+  >
+    <template v-slot:content>
+      <sort-content :active-id="mainActiveId" />
+    </template>
+  </van-tree-select>
 </template>
 
 <script>
-import SortSidebar from './components/SortSidebar'
+import { TreeSelect } from 'vant'
+import SortContent from './components/SortContent'
+import { getCategories } from '@/api'
 export default {
   name: 'Sort',
   components: {
-    SortSidebar
+    [TreeSelect.name]: TreeSelect,
+    SortContent
+  },
+  data: () => ({
+    mainActiveIndex: 0,
+    items: []
+  }),
+  computed: {
+    mainActiveId() {
+      if (this.items.length) return this.items[this.mainActiveIndex].id
+      return 1
+    }
+  },
+  created() {
+    this.getCategories()
+  },
+  methods: {
+    async getCategories() {
+      const { data } = await getCategories()
+      const items = data.map(item => ({ text: item.title, id: item.id }))
+      this.items = items
+    }
   }
 }
 </script>
+
+<style lang="less" scoped>
+/deep/.van-tree-select__nav-item{
+  height: 58px;
+  line-height: 58px;
+}
+
+/deep/.van-tree-select__content{
+  flex: 3;
+}
+</style>
