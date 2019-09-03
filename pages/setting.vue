d0009a8495d7ab34aa33be28304c681f38fd2002
luoyangC <luoyang15261829198@gmail.com>
2019-09-04 00:42:50 +0800

    fix: 示例链接跳转错误

--- pages/setting.vue
+++ pages/setting.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <van-cell-group>
-      <van-cell title="示例（一些小demo）" is-link clickable to="/page/about" />
+      <van-cell title="示例（一些小demo）" is-link clickable to="/about" />
       <van-cell title="文档（还在写/(ㄒoㄒ)/~~）" is-link clickable @click="$toast('尽请期待')" />
       <van-cell title="源码（就是github地址啦~\(≧▽≦)/~）" is-link clickable url="https://github.com/luoyangC/vue-mall-template" />
       <van-cell title="关于（作者的个人博客(*^__^*)）" is-link clickable url="https://www.luoyangc.cn" />
